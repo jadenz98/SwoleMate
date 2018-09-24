3b7b3b5c9f352a5edc95009aee3bc4f9232fad37
jadenz98 <zerbe@purdue.edu>
2018-09-24 11:05:35 -0400

    Set up react-navigation and started login screen
    App.js is the entry point of the app and initializes our stack for navigation. Once screens are created, they can be included as demonstrated in the comments.

--- Frontend/SwoleMate/App.js
+++ Frontend/SwoleMate/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+/*
+import screens to be used in the navigator in the following way
+import Homescreen from './.../Homescreen';
+import Loginscreen from './.../Homescreen';
+*/
 
+/*This is the first class that gets loaded, it basically takes the intial
+page and places it on the stack to be displayed*/
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
+
+//creates navigation stack (push when new page, pop when previous page requested)
+const RootStack = createStackNavigator({
+    /*{//different screens go here
+      Home: Homescreen,
+      Login: LoginScreen,
+    },
+    {//options go here
+      initialRouteName: 'Login',
+    }*/
+});
 
 const styles = StyleSheet.create({
   container: {
---
+++ Frontend/SwoleMate/Screens/LoginScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, TextView, TouchableOpacity } from 'react-navigation';
+
+export class LoginScreen extends React.Component {
+    render(){
+        
+    }
+}
