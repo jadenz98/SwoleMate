7e0f990094e1bd5362ffff2a4c0c81134da315c1
jadenz98 <zerbe@purdue.edu>
2018-09-24 21:36:23 -0400

    Modified StyleSheets and used in LoginScreen

--- Frontend/SwoleMate/Screens/LoginScreen.js
+++ Frontend/SwoleMate/Screens/LoginScreen.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
+//imports stylesheet for the LoginScreen
+import styles from './Styles/LoginScreenStyles';
+
 export default class LoginScreen extends React.Component {
+    //This sets the title on the top header
     static navigationOptions = {
         title: 'Login',
     };
     render(){
+        //inside of return is jsx style code that will be rendered on the page
         return(
             <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <TextInput placeholder='Username'/>
-                <TextInput placeholder='Password'/>
-                <TouchableOpacity style={{alignItems: 'center', padding: 5, backgroundColor: '#F9F9F9'}}>
-                    <Text >
+                <TextInput 
+                    placeholder='Username'
+                    style={styles.textbox}
+                />
+                <TextInput 
+                    placeholder='Password'
+                    style={styles.textbox}
+                />
+                {/*TouchableOpacity will be used as a button because it is more customizable and can funtion the same*/}
+                <TouchableOpacity style={styles.button}>
+                    <Text>
                         Login
                     </Text>
                 </TouchableOpacity>
---
+++ Frontend/SwoleMate/Screens/Styles/LoginScreenStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet} from 'react-native';
+
+//exports stylesheet to be used in other classes
+export default StyleSheet.create({
+    //name
+    textbox: {
+        //options
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    button: {
+        alignItems: 'center',
+        padding: 5,
+        backgroundColor: '#F9F9F9',
+    }
+});
