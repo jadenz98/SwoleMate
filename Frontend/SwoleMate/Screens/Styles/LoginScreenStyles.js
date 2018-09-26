372873f99731ce3b2a1b02bb1e1450c3d018923e
jadenz98 <zerbe@purdue.edu>
2018-09-26 11:15:41 -0400

    A little styling on the buttons and textboxes

--- Frontend/SwoleMate/Screens/Styles/LoginScreenStyles.js
+++ Frontend/SwoleMate/Screens/Styles/LoginScreenStyles.js
@@ -5,12 +5,15 @@ export default StyleSheet.create({
     //name
     textbox: {
         //options
+        width: 150,
         borderColor: 'black',
         borderWidth: 1,
     },
     button: {
+        width: 75,
         alignItems: 'center',
         padding: 5,
+        borderColor: 'black',
         backgroundColor: '#F9F9F9',
-    }
+    },
 });
