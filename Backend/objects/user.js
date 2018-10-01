8c1eafd8afc7057af6eab3f7a5e21a6cf883d716
Ryan Sand <rsand3642@gmail.com>
2018-09-30 20:42:05 -0400

    updated user properties

--- Backend/objects/user.js
+++ Backend/objects/user.js
@@ -1,7 +1,15 @@
 export default class User {
     username;
+    pass;
+    email;
+    DOB;
+    phone;
 
-    constructor (username) {
+    constructor (username, pass, email, DOB, phone) {
         this.username = username;
+        this.pass = pass;
+        this.email = email;
+        this.DOB = DOB;
+        this.phone = phone;
     }
 }
