cb4b08687ec71dd0639b9e059f91e0268c39b4bb
jadenz98 <zerbe@purdue.edu>
2018-10-04 01:37:57 -0400

    Created Multiple Selection for User interests in EditProfile

--- Frontend/SwoleMate/Screens/EditProfile.js
+++ Frontend/SwoleMate/Screens/EditProfile.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import {View, Text, Modal, TouchableHighlight } from 'react-native';
+import SelectMultiple from 'react-native-select-multiple'
+
+const interests = ['Biking', 'Running', 'Swimming']
 
 export default class EditProfile extends React.Component{
-    
+
+    state = {
+        selectedInterests: [],
+        modalVisible: false,
+    }
         //This sets the title on the top header
         static navigationOptions = {
             title: 'Edit',
         };
     
     render(){
-        return null;
+        return(
+            <View style={{marginTop: 10}}>
+                <Modal 
+                    transparent={false} 
+                    visible={this.state.modalVisible}>
+                    <View style={{marginTop: 22}}>
+                        <SelectMultiple
+                            items={interests}
+                            selectedItems={this.state.selectedInterests}
+                            onSelectionsChange={this.onSelectionsChange}
+                        />
+                        <TouchableHighlight
+                            onPress={() => {
+                                this.setModalVisibility(!this.state.modalVisible);
+                            }}>
+                            <Text>Hide Interests</Text>
+                        </TouchableHighlight>
+                    </View>
+                </Modal>
+                <TouchableHighlight
+                    onPress={() => {
+                        this.setModalVisibility(true);
+                    }}>
+                    <Text>Show Interests</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+
+    onSelectionsChange = (selectedInterests) => {
+        this.setState({ selectedInterests });
+    }
+
+    setModalVisibility(visible) {
+        this.setState({ modalVisible: visible});
     }
 }
