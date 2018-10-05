9eb2ea1774eb34cdcdae4e8af2290b73171c57ca
Ryan Sand <rsand3642@gmail.com>
2018-10-05 00:15:04 -0400

    added function that retrieves a conversation in frontend

--- Frontend/SwoleMate/Screens/Messsages.js
+++ Frontend/SwoleMate/Screens/Messsages.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import styles from "./Styles/LoginScreenStyles";
+
+import { Text, View, TextInput, TouchableOpacity, Picker, Modal, TouchableHighlight } from 'react-native';
+import SelectMultiple from 'react-native-select-multiple';
+
+import Connector from '../Utils/Connector';
 
 export default class Messages extends React.Component{
+
+  getConversation = () => {
+    Connector.get('/user/conversation', {email1: 'sam@samingram.me', email2: 'asdf@asdf.com'}, (conversation) => {
+        console.log(conversation);
+    });
+  }
+
     render(){
         return null;
     }
