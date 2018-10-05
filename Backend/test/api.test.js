8348b61b289054c7bdb77626234a8b0b1b4cb885
Steven Dellamore <stevendellamore@pal-nat186-63-187.itap.purdue.edu>
2018-10-05 10:01:49 -0400

    added ROBUST test and commented all of the tests

--- Backend/test/api.test.js
+++ Backend/test/api.test.js
@@ -8,7 +8,9 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('SwoleMate API endpoint testing', () => {
-    // it('Deleting old User', (done) => {
+    /*
+    Creats a newUser with correct username and pass
+    */
     newUser = {
         name: 'mocha-test',
         password: 'test',
@@ -17,14 +19,24 @@ describe('SwoleMate API endpoint testing', () => {
         phone: '1234567890',
         bio: 'test bio!'
     };
-    //     chai.request(server)
-    //         .get('user/delete')
-    //         .send(newUser)
-    //         .end((err, res) => {
-    //             res.should.have.status(200);
-    //             done();
-    //         });
-    // });
+    /*
+    Creats a badUser with incorrect username and pass
+    */
+    badUser = {
+        name: 'mocha-test',
+        password: 'test1',
+        email: 'test@test.com1',
+        birthday: '01/01/01',
+        phone: '1234567890',
+        bio: 'test bio!'
+    };
+    /*
+    This is for testing purposes, deletes the user if he is already
+    in the database, does not expect anything besides a 200 from the server 
+    and a json object back. (does not check if true or false)
+    */
+
+
     it('Delete User for re-testing', (done) => {
         chai.request(server)
             .post('/user/delete')
@@ -36,7 +48,9 @@ describe('SwoleMate API endpoint testing', () => {
             });
 
     });
-
+    /*
+    This test sends a get request to the server
+    */
     it('Should handle a get request', (done) => {
         chai.request(server)
             .get('/hello')
@@ -51,7 +65,9 @@ describe('SwoleMate API endpoint testing', () => {
                 done();
             });
     });
-
+    /*
+    This test creates a user and then deletes the user.
+    */
     it('Should be able to delete a user', (done) => {
         const newUser = {
             name: 'mocha-test',
@@ -82,16 +98,13 @@ describe('SwoleMate API endpoint testing', () => {
                     });
             });
     });
-    // const newUser = {
-    //         name: 'mocha-test',
-    //         password: 'test',
-    //         email: 'test@test.com',
-    //         sex: 'Male',
-    //         birthday: '01/01/01',
-    //         phone: '1234567890',
-    //         bio: 'test bio!'
-    //     };
 
+    /*
+    This test checks the login to with
+    bad email + good pass. -> false
+    good email + bad pass  -> false
+    bad email + bad pass. -> false
+    */
     it('Should be able to block wrong login creds', (done) => {
         const badPass = {
             name: 'mocha-test',
@@ -154,6 +167,9 @@ describe('SwoleMate API endpoint testing', () => {
                     done();
             });
     });
+    /*
+    This test logs the user in with the correct email + password
+    */
     it('Should be able to Login with valid email + password', (done) => {
         chai.request(server)
             .post('/user/login')
@@ -165,6 +181,9 @@ describe('SwoleMate API endpoint testing', () => {
             });
 
     });
+    /*
+    This test updating location (does not actaully check if its right though :( )
+    */
 
     it('Should be able update Location', (done) => {
         chai.request(server)
@@ -178,6 +197,55 @@ describe('SwoleMate API endpoint testing', () => {
             });
     });
 
+    /*
+    This deletes the user -> registers a user 
+    -> trys to log in with wrong email and pass
+    -> logs in with correct email and pass
+    */
+
+    it('ROBUSTNESS', (done) => {
+        chai.request(server)
+            .post('/user/delete')
+            .send({email: newUser.email})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.success.should.be.true;
+                chai.request(server)
+                    .post('/user/register')
+                    .send(newUser)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.an('object');
+                        res.body.success.should.be.true;
+
+                        chai.request(server)
+                            .post('/user/login')
+                            .send(badUser)
+                            .end((err, res) => {
+                                res.should.have.status(200);
+                                res.body.should.be.an('object');
+                                res.body.success.should.be.false;
+                                chai.request(server)
+                                    .post('/user/login')
+                                    .send(newUser)
+                                    .end((err,res) => {
+                                        res.should.have.status(200);
+                                        res.body.should.be.an('object');
+                                        res.body.success.should.be.true;
+                                        done();
+                                    })
+                            });
+                    
+                    });
+            });
+    });
+    /*
+        Frontend testing??
+    */
+    it('Should be able to logout after being logged in', (done) => {
+        done();
+    });
 
 });
 
