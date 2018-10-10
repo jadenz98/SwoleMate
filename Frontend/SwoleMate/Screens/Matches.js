9778b165e8e61d04713d6d7e9797cdbe1ad41d9f
jadenz98 <zerbe@purdue.edu>
2018-10-09 20:35:36 -0400

    Successfully grab photo data
    I have also started a Flatlist for the user matches

--- Frontend/SwoleMate/Screens/Matches.js
+++ Frontend/SwoleMate/Screens/Matches.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import styles from "./Styles/LoginScreenStyles";
 
-import { Text, View, TextInput, TouchableOpacity, Picker, Modal, TouchableHighlight } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, Picker, Modal, TouchableHighlight, FlatList } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
+
 import SelectMultiple from 'react-native-select-multiple';
 
 import Connector from '../Utils/Connector';
 
 export default class Matches extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            data: null
+        }
+    }
 
   getMatches = () => {
     Connector.get('/user/matches', {email: 'sam@samingram.me'}, (matches) => {
@@ -20,10 +29,25 @@ export default class Matches extends React.Component{
         };
 
     render(){
-        return(<TouchableOpacity onPress={this.getMatches}>
-          <Text>
-            See Matches
-          </Text>
-        </TouchableOpacity>);
+        return(
+            <View>
+                <FlatList
+                    
+                    data={[
+                        {key: 'Jaden'},
+                        {key: 'Sam'},
+                        {key: 'Steven'},
+                        {key: 'Ryan'},
+                        {key: 'Kevin'},
+                    ]}
+                    renderItem={({item}) =>
+                        <Text>
+                            {item.key}
+                        </Text>
+                    }
+                />
+            </View>
+        );
+          
     }
 }
--- Frontend/SwoleMate/Screens/PickPhoto.js
+++ Frontend/SwoleMate/Screens/PickPhoto.js
@@ -10,17 +10,35 @@ import Connector from '../Utils/Connector';
 
 export default class PickPhoto extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state= {
+            photo: null
+        }
+    }
+
     static navigationOptions = {
         title: 'Pick Photo',
     };
 
-    getSelectedImages(image){
-        
+    getSelectedImages = (image) =>{
+        //image is an array of image object
+        //the state photo is set to the image object including the uri
+        this.setState({photo: image[0]});
+    }
+
+    save = () => {
+    
     }
 
     render(){
         return(
             <View style={{flex: 1}}>
+                <TouchableOpacity onPress={this.save}>
+                    <Text>
+                        Save Picture
+                    </Text>
+                </TouchableOpacity>
                 <CameraRollPicker callback={this.getSelectedImages} maximum={1}/>
             </View>
         );
