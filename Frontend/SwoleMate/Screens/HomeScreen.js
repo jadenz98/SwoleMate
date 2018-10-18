519e9de5193b99f98aed692a7e1720be02a0a390
jadenz98 <zerbe@purdue.edu>
2018-10-18 00:59:19 -0400

    Added Card Swiper to HomeScreen

--- Frontend/SwoleMate/Screens/HomeScreen.js
+++ Frontend/SwoleMate/Screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {TouchableOpacity, View, Text, Image} from 'react-native';
+import {TouchableOpacity, View, Text, Image, Button, StyleSheet} from 'react-native';
 import styles from './Styles/HomeScreenStyles';
-
+import Swiper from 'react-native-deck-swiper';
 
 export default class HomeScreen extends React.Component{
     constructor(props){
@@ -10,27 +10,62 @@ export default class HomeScreen extends React.Component{
         const name = navigation.getParam('username');
     }
     //This sets the title on the top header
-    static navigationOptions = {
-        title: 'SwoleMate',
-    };
+    static navigationOptions = ({ navigation }) => {
+        return{
+            title: 'SwoleMate',
+            headerRight: (
+                <TouchableOpacity  onPress={() => navigation.navigate('Matches')}>
+                  <Text>
+                      Matches
+                  </Text>
+              </TouchableOpacity>),
+            headerLeft: (
+              <TouchableOpacity style={styles.button} onPress ={ () => {
+                //this alert tests that username was successfully recieved from previous page
+                //alert('Username: ' + name);
+                
+                var userinfo={
+                    email: navigation.getParam('email'),
+                    interests: navigation.getParam('interests')
+                };
+                navigation.navigate('Profile', userinfo);
+            }}>
+              {/*<Text>
+                  Profile
+              </Text>*/}
+              <Image
+                  style={styles.icon}
+                  source={require('./generic-profile-picture.png')}
+              />
+                </TouchableOpacity>
+              ),
+        };
+    }
 
     render(){
         return(
           <View>
-            <TouchableOpacity style={styles.button} onPress={this.goToProfile}>
-                    <Text>
-                        Profile
-                    </Text>
-                    {/*<Image
-                        style={styles.icon}
-                        source={require('./generic-profile-picture.png')}
-                    />*/}
-                </TouchableOpacity>
-                <TouchableOpacity onPress={this.goToMatches}>
-                  <Text>
-                    Matches
-                  </Text>
-                </TouchableOpacity>
+            <Swiper
+                cards={[{word:'Hello', otherWord:'World'},{word:'Goodbye', otherWord:'World'}]}
+                
+                //stackSize={2}
+                renderCard={(card) => {
+                    return (
+                        <View style={styles.card}>
+                            <Text> {card.word}{card.otherWord} </Text>
+                        </View>
+                    )
+                }}
+                onSwiped={(cardIndex) => {console.log(cardIndex)}}
+                onSwipedAll={() => {console.log('No More Potential Matches')}}
+                cardIndex={0}
+                backgroundColor={'#45a1e8'}
+                cardVerticalMargin={20}
+                marginTop={0}
+                marginBottom={120}
+                >
+            </Swiper>
+
         </View>
         );
     }
